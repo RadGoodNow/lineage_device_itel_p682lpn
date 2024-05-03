1831939c55940e04a477f1f953d33b1180cd07e3
SCARYGOOSE <81907382+RadGoodNowYT@users.noreply.github.com>
2024-05-03 19:24:23 +0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,26 +1,11 @@
 # Inherit from t310-common
 $(call inherit-product, device/itel/P682LPN/P682LPN.mk)
 
-# Vendor
-$(call inherit-product-if-exists, vendor/retroid/pocket2plus/pocket2plus-vendor.mk)
-
-# Vendor apps
-$(call inherit-product-if-exists, vendor/retroid/apps/pocket2plus-apps.mk)
-
 # Fix symlinks
 $(call inherit-product, $(LOCAL_PATH)/fixsymlinks/fixsymlinks.mk)
 
-# Vendor Prop
-include $(LOCAL_PATH)/vendor_prop.mk
-
-# MicroSD boot files
-PRODUCT_COPY_FILES += \
-	device/retroid/pocket2plus/microsdboot/device-resources/deviceinfo:root/device-resources/deviceinfo
 
 # Recovery files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/ueventd.ums312_2h10.rc:root/ueventd.ums312_2h10.rc \
     $(LOCAL_PATH)/recovery/init.recovery.ums312_2h10.rc:root/init.recovery.ums312_2h10.rc \
-
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay/aosp
