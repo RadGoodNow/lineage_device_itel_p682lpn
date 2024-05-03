ef966f8087c74f416df7c428ac57e437aea8fe50
SCARYGOOSE <81907382+RadGoodNowYT@users.noreply.github.com>
2024-05-03 19:54:47 +0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,2 @@
 # Inherit from t310-common
-$(call inherit-product, device/itel/P682LPN/P682LPN.mk)
-
-# Fix symlinks
-$(call inherit-product, $(LOCAL_PATH)/fixsymlinks/fixsymlinks.mk)
-
-
-# Recovery files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/ueventd.ums312_2h10.rc:root/ueventd.ums312_2h10.rc \
-    $(LOCAL_PATH)/recovery/init.recovery.ums312_2h10.rc:root/init.recovery.ums312_2h10.rc \
+$(call inherit-product, device/itel/P682LPN/lineage_P682LPN.mk)
